e22f565b6df522611d44b2fdd39748168efc7baf
madaboutcode <madaboutcode@users.noreply.github.com>
2026-04-21 20:14:45 +0530

    Fix startserver.sh and add exclude rules to _config.yml

--- startserver.sh
+++ startserver.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo Visit http://localhost:4000 in your browser to see the blog
-jekyll serve --watch --incremental --future $@
+export PATH="/opt/homebrew/Cellar/ruby/3.4.8/bin:$PATH"
+bundle exec jekyll serve --livereload --incremental --future "$@"
